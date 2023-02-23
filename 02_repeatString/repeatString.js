a48042dbc59da1508e681e9bd4854539298517b3
NTgra <nektotigra@gmail.com>
2023-02-23 12:51:20 -0700

    Add the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
 
+
+const repeatString = function(word, times) {
+    let string ="";
+    for (let i=0; i<times; i++){
+        string+=word;
+    } if (times<0){
+        return "ERROR";
+    }
+    return string;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;

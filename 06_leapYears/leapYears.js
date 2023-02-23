3aea793387b34d1c72645f5cb9da9a2adec60f07
NTgra <nektotigra@gmail.com>
2023-02-23 16:55:22 -0700

    Add the leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year%4===0&&year%100===0){
+        if (year%400===0){
+            return true;
+        }
+        return false;
+    }
+    if (year%4===0||year%400===0){
+        return true
+    } else return false;
 };
-
+leapYears(2000) // is a leap year: returns true
+leapYears(1985) // is not a leap year: returns false
+leapYears(1996)
 // Do not edit below this line
 module.exports = leapYears;

883bdcb2a4b4e271f63e40f85431de68c3e0675f
NTgra <nektotigra@gmail.com>
2023-02-23 13:14:38 -0700

    Add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let outputString='';
+
+    for (let i=1; i<=word.length;i++){
+        outputString+=word.at(-i)
+    }
+
+    return outputString;
 };
+reverseString('hello there') // returns 'ereht olleh'
+                                           
 
 // Do not edit below this line
 module.exports = reverseString;

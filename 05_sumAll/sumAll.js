2583a27bf9d8fc9276344ece4a1672fa3f3e4f77
NTgra <nektotigra@gmail.com>
2023-02-23 16:06:33 -0700

    Add the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let theSum = 0;
+    if(first<0||second<0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(first) || !Number.isInteger(second)){
+        return "ERROR";
+    }
+    if(first>second){
+        let start=second;
+        let end=first;
+        for(let i=start; i<=end; i++){
+            theSum+=i;
+        }
+    } else {
+        let start=first;
+        let end = second;
+        for(let i=start; i<=end; i++){
+            theSum+=i;
+        }
+    }
+    
+    return theSum;
 };
+sumAll(123, 1)
 
 // Do not edit below this line
 module.exports = sumAll;

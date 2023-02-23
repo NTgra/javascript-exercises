0646d0d2ed66ab8d0e422d42da80facd124cc693
NTgra <nektotigra@gmail.com>
2023-02-23 15:42:37 -0700

    Add removeFromArray method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {    
+   const newArray = [];
+   array.forEach(element => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+   });
+   console.log(newArray) 
+   return newArray;
 };
-
+removeFromArray([1, 2, 3, 4], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;

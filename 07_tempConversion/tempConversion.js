af71ff5d950a2b7bc78a8bb2da970d0616b154fe
NTgra <nektotigra@gmail.com>
2023-02-23 18:18:13 -0700

    Add both conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celciusTemp) {
+  
+  let fahrenheit = (celciusTemp-32)*0.5556;
+  if (fahrenheit===0){
+    return fahrenheit;
+  } else {
+    return Math.round( fahrenheit * 1e1 ) / 1e1
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheitTemp) {
+  let celsius = (fahrenheitTemp*(9/5))+32;
+  return Math.round(celsius * 1e1 ) / 1e1;
 };
+convertToCelsius(-100) // fahrenheit to celsius, should return 0
 
+convertToFahrenheit(0) // celsius to fahrenheit, should return 32
 // Do not edit below this line
 module.exports = {
   convertToCelsius,

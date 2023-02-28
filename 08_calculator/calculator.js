14d75ac4b7166479e4f35b570b4ee0fac313c4d7
NTgra <nektotigra@gmail.com>
2023-02-28 16:07:25 -0700

    Solve the Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return parseInt(first)+parseInt(second);
 };
 
-const subtract = function() {
-	
+const subtract = function(first,second) {
+	return parseInt(first)-parseInt(second);
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((partialSum, a) => partialSum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  return myArray.reduce((product, a) => product * a, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number===0||number===1){
+    return 1;
+  }
+  if (number>1){
+    let factorial=number;
+    for (let i=number-1;i>0;i--){
+      factorial*=i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line

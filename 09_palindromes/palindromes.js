6a3f1618a44e087570f30e14f42db03b79d879c8
NTgra <nektotigra@gmail.com>
2023-02-28 17:01:57 -0700

    Solve the functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (value) {
+    let caseInsensetive = value.toUpperCase();
+    let str = caseInsensetive.replace(/\s|,|\.|!/gi, '');
+    let i=str.length-1;
+    let matchesNeeded=i/2;
+    let matches = 0;
+    
+    for (let j=0; j<i/2; j++){
+        if(str[j]===str[i-j]){
+            matches++;
+            
+        }
+    }
+    if (matches>=matchesNeeded){ 
+        return true;
+    }
+    else {  
+        return false;
+    }
 };
 
 // Do not edit below this line

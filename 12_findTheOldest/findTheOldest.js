4b0b543c3e587a0a874d3cec18bcde3726bb2e6b
NTgra <nektotigra@gmail.com>
2023-03-01 19:36:07 -0700

    Add the solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    const theOldest = people.sort(function(a,b){
+        if (typeof a.yearOfDeath === 'undefined'){
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (typeof b.yearOfDeath === 'undefined'){
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+   return theOldest[0];
 };
 
 // Do not edit below this line
